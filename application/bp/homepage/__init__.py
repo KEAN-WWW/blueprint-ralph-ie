81fc6887bd6b6ccb558b046041e0b973662bab21
ralph-ie <colmenra@kean.edu>
2025-11-18 23:43:15 -0500

    Fix blueprint files and autograder setup

--- application/bp/homepage/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from .routes import bp  # replace with your actual blueprint file
-
-# create the Flask app
-app = Flask(__name__)
-
-# register your blueprint
-app.register_blueprint(bp)
